1823f6767b4b208c857ed60caf56486e5e6ae2c1
JIH0LEE <skql5810@gmail.com>
2022-08-21 18:30:46 +0900

    make category menu

--- frontend/src/page/MainPage/index.js
+++ frontend/src/page/MainPage/index.js
@@ -9,12 +9,33 @@ function MainPage() {
   const [modalVisible, setModalVisible] = useState(false);
   const [items, setItems] = useState([]);
   const [clickItem, setClickItem] = useState(0);
+  const categories = [
+    "전체보기",
+    "샐러드·닭가슴살",
+    "도시락·밥류",
+    "떡볶이·튀김·순대",
+    "피자·핫도그·만두",
+    "죽·스프·카레",
+    "폭립·떡갈비·안주",
+    "파스타·면류",
+    "선식·시리얼",
+    "레드와인",
+    "화이트와인",
+    "샴페인",
+    "막걸리·탁주",
+    "증류주·약주·청주",
+    "생수·탄산수",
+    "음료·주스",
+    "우유·두유·요거트",
+    "커피",
+    "차",
+  ];
+  const [selectedCategory, setSelectedCategory] = useState("전체보기");
 
   useEffect(() => {
     axios
       .get(API_SERVER + "/items")
       .then((res) => {
-        console.log(res.data);
         setItems(res.data);
       })
       .catch((err) => {
@@ -22,6 +43,32 @@ function MainPage() {
       });
   }, []);
 
+  useEffect(() => {
+    if (selectedCategory === "전체보기") {
+      axios
+        .get(API_SERVER + "/items")
+        .then((res) => {
+          setItems(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      console.log(API_SERVER + `/items/filter?category=${selectedCategory}`);
+      axios
+        .get(API_SERVER + `/items/filter?category=${selectedCategory}`)
+        .then((res) => {
+          setItems(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [selectedCategory]);
+
+  const categoryHandler = (e) => {
+    setSelectedCategory(e.target.value);
+  };
   const changePriceFormat = (price) => {
     return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   };
@@ -30,7 +77,20 @@ function MainPage() {
     <div className="main-page-container">
       <div className="recommend-container">
         <div className="label-container">
-          <div>밀키트를 골라주세요</div>
+          <div>상품을 골라주세요</div>
+        </div>
+        <div className="category-container">
+          {categories.map((category) => (
+            <button
+              className="category"
+              value={category}
+              key={category}
+              onClick={categoryHandler}
+            >
+              {" "}
+              {category}
+            </button>
+          ))}
         </div>
         <div className="items-container">
           {items.map((item) => (
