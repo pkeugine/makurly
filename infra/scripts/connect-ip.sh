a32e1024a5f0c16e45dc2ca20ff75cc5feba1a0d
Eugine Park <pkeugine@gmail.com>
2022-08-24 02:51:48 +0900

    hide crutial info and change settings

--- infra/scripts/connect-ip.sh
+++ infra/scripts/connect-ip.sh
@@ -2,9 +2,9 @@ echo "REACT_APP_API_SERVER=https://$PK_API_DOMAIN_NAME" > $PK_INFRA_PATH/product
 echo "spring:" > $PK_INFRA_PATH/production/application-prod.yml
 echo "  datasource:" >> $PK_INFRA_PATH/production/application-prod.yml
 echo "    driver-class-name: org.mariadb.jdbc.Driver" >> $PK_INFRA_PATH/production/application-prod.yml
-echo "    url: jdbc:mariadb://$PK_DATABASE_PUBLIC_IP:3306/makurly?userSSL=false&serverTimezone=Asia/Seoul&characterEncoding=UTF-8" >> $PK_INFRA_PATH/production/application-prod.yml
-echo "    username: makurly" >> $PK_INFRA_PATH/production/application-prod.yml
-echo "    password: makurlypassword" >> $PK_INFRA_PATH/production/application-prod.yml
+echo "    url: jdbc:mariadb://$PK_DATABASE_PUBLIC_IP:$PK_DATABASE_PORT/$PK_DATABASE_DATABASE?userSSL=false&serverTimezone=Asia/Seoul&characterEncoding=UTF-8" >> $PK_INFRA_PATH/production/application-prod.yml
+echo "    username: $PK_DATABASE_USERNAME" >> $PK_INFRA_PATH/production/application-prod.yml
+echo "    password: $PK_DATABASE_PASSWORD" >> $PK_INFRA_PATH/production/application-prod.yml
 echo "  jpa:" >> $PK_INFRA_PATH/production/application-prod.yml
 echo "    hibernate:" >> $PK_INFRA_PATH/production/application-prod.yml
 echo "      ddl-auto: validate" >> $PK_INFRA_PATH/production/application-prod.yml
