79858c7fde46f96a3f4bf7c0e951c833eb6eeafc
JIH0LEE <skql5810@gmail.com>
2022-08-24 08:35:05 +0900

    change footer css

--- frontend/src/page/DashboardPage/index.js
+++ frontend/src/page/DashboardPage/index.js
@@ -4,15 +4,22 @@ function DashboardPage() {
   return (
     <div className="dashboard-page">
       <div
-        style={{ fontWeight: "bold", fontSize: "30px", color: "rgb(95,0,128)" }}
+        style={{
+          fontWeight: "bold",
+          fontSize: "30px",
+          color: "rgb(95,0,128)",
+          padding: "20px",
+        }}
       >
         Dashboard
       </div>
       <iframe
         title="dashboard"
-        style={{ border: "none" }}
+        style={{
+          border: "none",
+        }}
         src="https://public.tableau.com/views/makurly/1?:language=ko-KR&publish=yes&:display_count=n&:origin=viz_share_link?:embed=yes&:showVizHome=no"
-        width="1050"
+        width="1000"
         height="900"
       ></iframe>
     </div>
