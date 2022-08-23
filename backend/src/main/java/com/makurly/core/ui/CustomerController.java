fe0f8e24a72ae26a83d504d9b59b9d81c0054459
Eugine Park <pkeugine@gmail.com>
2022-08-24 03:55:00 +0900

    refactor customer controller

--- backend/src/main/java/com/makurly/core/ui/CustomerController.java
+++ backend/src/main/java/com/makurly/core/ui/CustomerController.java
@@ -3,7 +3,7 @@ package com.makurly.core.ui;
 import com.makurly.core.application.CustomerService;
 import com.makurly.core.ui.dto.CustomerRequest;
 import com.makurly.core.ui.dto.CustomerResponse;
-import org.springframework.http.HttpStatus;
+import java.net.URI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,25 +24,20 @@ public class CustomerController {
 
     @PostMapping
     public ResponseEntity<CustomerResponse> createCustomer(@RequestBody CustomerRequest customerRequest) {
-        CustomerResponse responseBody = customerService.createCustomer(customerRequest);
-        return ResponseEntity
-            .status(HttpStatus.CREATED)
-            .body(responseBody);
+        CustomerResponse customerResponse = customerService.createCustomer(customerRequest);
+        URI uri = URI.create(String.format("/%d", customerResponse.getId()));
+        return ResponseEntity.created(uri).body(customerResponse);
     }
 
     @PostMapping("/sign-in")
     public ResponseEntity<CustomerResponse> signIn(@RequestBody CustomerRequest customerRequest) {
-        CustomerResponse responseBody = customerService.signIn(customerRequest.getName());
-        return ResponseEntity
-            .status(HttpStatus.OK)
-            .body(responseBody);
+        CustomerResponse customerResponse = customerService.signIn(customerRequest.getName());
+        return ResponseEntity.ok(customerResponse);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<CustomerResponse> findCustomerById(@PathVariable Long id) {
-        CustomerResponse responseBody = customerService.getCustomerById(id);
-        return ResponseEntity
-            .status(HttpStatus.OK)
-            .body(responseBody);
+        CustomerResponse customerResponse = customerService.getCustomerById(id);
+        return ResponseEntity.ok(customerResponse);
     }
 }
