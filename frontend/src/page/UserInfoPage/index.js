c36d236fc61ac8202769dd765a2460d0fc7522d3
JIH0LEE <skql5810@gmail.com>
2022-08-24 08:21:30 +0900

    add dashboard page

--- frontend/src/page/UserInfoPage/index.js
+++ frontend/src/page/UserInfoPage/index.js
@@ -14,6 +14,7 @@ function UserInfoPage() {
       setInteractions(res.data);
       // console.log(interactions);
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
